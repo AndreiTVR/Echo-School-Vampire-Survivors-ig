1a820016a7f3f774d8c96823a8085b345262c541
cristianaopritoiu-lab <cristiana.opritoiu@yahoo.com>
2026-02-16 15:38:09 +0200

    .
    ZombieController added

---
+++ Assets/Scripts/ZombieController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour
+{
+    
+    public int health = 250;
+    public int damage = 10;
+    public float attackSpeed = 1.0f; 
+    public int nectarDropAmount = 2; 
+
+    private float nextAttackTime;
+
+    
+    private void OnCollisionStay(Collision collision)
+    {
+        
+        PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+
+        if (player != null && Time.time >= nextAttackTime)
+        {
+            player.TakeDamage(damage); 
+            nextAttackTime = Time.time + attackSpeed; 
+        }
+    }
+
+    
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Zombie mort! Ai primit: " + nectarDropAmount + " nectar");
+        Destroy(gameObject);
+    }
+}
