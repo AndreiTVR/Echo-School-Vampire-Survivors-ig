d947e73c4f568da49a9860e02f8ad0d88931eac8
AndreiTVR <andreiteianu2000@gmail.com>
2026-02-16 15:52:54 +0200

    .

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,8 +3,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public int health = 100;
-    public int damage = 10;
-   [SerializeField] int maxHealth = 100;
+    [SerializeField] int maxHealth = 100;
+
+    [Header("Shooting Settings")]
+    public GameObject projectilePrefab; 
+    public Transform firePoint;        
+    public float fireRate = 0.5f;    
+
+    private float nextFireTime = 0f;   
+
     public void TakeDamage(int damage)
     {
         health -= damage;
@@ -12,5 +19,20 @@ public class PlayerController : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= nextFireTime)
+        {
+            Shoot();
+            nextFireTime = Time.time + fireRate;
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+        Debug.Log("Shot fired!");
     }
 }
