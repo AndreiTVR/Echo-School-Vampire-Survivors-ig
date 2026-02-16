329b6332aedc27591f5bc1177607cfd920e395df
AndreiTVR <andreiteianu2000@gmail.com>
2026-02-16 17:01:54 +0200

    .

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -5,7 +5,7 @@ public class BulletController : MonoBehaviour
     public float speed = 10f;
     public float maxDistance = 40f;
 
-    private int damageToDeal;
+    public int damageToDeal;
     private Vector2 startPosition; // Changed to Vector2 for 2D
 
     void Start()
