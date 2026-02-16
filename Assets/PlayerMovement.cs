d161ee9966e763f68f6cfd48b41870e3810dc2df
cristianaopritoiu-lab <cristiana.opritoiu@yahoo.com>
2026-02-16 15:16:04 +0200

    .
    PlayerMovement added

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveX, 0, moveZ);
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   
+}
