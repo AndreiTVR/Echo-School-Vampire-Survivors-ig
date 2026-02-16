319cf2cdd33fa6523bf80550506cccad34b3d7df
cristianaopritoiu-lab <cristiana.opritoiu@yahoo.com>
2026-02-16 20:35:09 +0200

    Update NectarTex01.cs

--- Assets/Scripts/NectarTex01.cs
+++ Assets/Scripts/NectarTex01.cs
@@ -1,17 +1,31 @@
 using UnityEngine;
-using TMPro; 
+using TMPro;
 
 public class NectarDisplay : MonoBehaviour
 {
-    public TextMeshProUGUI textMesh; 
-    public PlayerController player;  
-    public int nectarTarget = 10;    
+    public TextMeshProUGUI textMesh;
+    public PlayerController player;
+    public int nectarTarget = 10;
+
+    
+    public Color culoareNormala = Color.white;
+    public Color culoareFinala = Color.green;
+
     void Update()
     {
         if (player != null)
         {
-            
-            textMesh.text = "Nectar: " + player.nectarCollected + " / " + nectarTarget;
+            textMesh.text = "NECTAR: " + player.nectarCollected + " / " + nectarTarget;
+
+            if (player.nectarCollected >= nectarTarget)
+            {
+                textMesh.color = culoareFinala;
+                textMesh.fontSize = 30; 
+            }
+            else
+            {
+                textMesh.color = culoareNormala;
+            }
         }
     }
 }
