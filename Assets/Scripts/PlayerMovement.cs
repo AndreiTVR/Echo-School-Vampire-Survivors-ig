446f306d24428240bfb2505d9be672cd3bcc801d
AndreiTVR <andreiteianu2000@gmail.com>
2026-02-17 14:38:10 +0200

    ,

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,12 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
-
+    Animator anim;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
     }
 
     void Update()
@@ -21,10 +23,45 @@ public class PlayerMovement : MonoBehaviour
 
         if (keyboard != null)
         {
-            if (keyboard.aKey.isPressed) x = -1f;
-            if (keyboard.dKey.isPressed) x = 1f;
-            if (keyboard.wKey.isPressed) y = 1f;
-            if (keyboard.sKey.isPressed) y = -1f;
+            if (keyboard.aKey.isPressed)
+            {
+                anim.SetBool("backWalk", false);
+                anim.SetBool("sideskWalk", false);
+                anim.SetBool("walkFront", true);
+                anim.SetBool("idle", false);
+
+                x = -1f;
+            }
+            if (keyboard.dKey.isPressed)
+            {
+                x = 1f;
+                anim.SetBool("backWalk", false);
+                anim.SetBool("sideskWalk", true);
+                anim.SetBool("walkFront", false);
+                anim.SetBool("idle", false);
+            }
+            if (keyboard.wKey.isPressed)
+            {
+                y = 1f;
+                anim.SetBool("backWalk", true);
+                anim.SetBool("sideskWalk", false);
+                anim.SetBool("walkFront", false);
+                anim.SetBool("idle", false);
+            }
+            if (keyboard.sKey.isPressed) {
+                y = -1f;
+                anim.SetBool("backWalk", false);
+                anim.SetBool("sideskWalk", true);
+                anim.SetBool("walkFront", false);
+                anim.SetBool("idle", false);
+            }
+            else
+            {
+                anim.SetBool("backWalk", false);
+                anim.SetBool("sideskWalk", false);
+                anim.SetBool("walkFront", false);
+                anim.SetBool("idle", true);
+            }
         }
 
         moveInput = new Vector2(x, y).normalized;
